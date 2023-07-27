42dd32e163afad7d155162bab099c4dcf4102f92
milton cañari <raivane.entity@gmail.com>
2023-07-26 21:07:40 -0500

    añadido sourcemaps y cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,41 @@ const { src, dest, watch, series, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-const imagenmin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif')
+
+const sourcemaps = require("gulp-sourcemaps");
+const cssnano= require('cssnano')
+
+const imagenmin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 function css(done) {
   src("src/scss/app.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: "expanded" }))
-    .pipe(postcss([autoprefixer()]))
+    .pipe(postcss([autoprefixer(),cssnano()]))
+    .pipe(sourcemaps.write("."))
     .pipe(dest("build/css"));
   done();
 }
 function imagenes() {
   return src("src/img/**/*")
     .pipe(imagenmin({ optimizationLevel: 3 }))
-    .pipe(dest('build/img'))
+    .pipe(dest("build/img"));
 }
 function imageneswebp() {
-  return src('src/img/**/*.{png,jpg}')
-    .pipe(webp())
-    .pipe(dest('build/img'))
+  return src("src/img/**/*.{png,jpg}").pipe(webp()).pipe(dest("build/img"));
 }
 function imagenesavif() {
-  const opciones = 
-    {
-      quality: 50,
-      lossless: false,
-      speed: 0,
-      chromaSubsampling: '4:2:0'
-  }
-  
-  return src('src/img/**/*.{png,jpg}')
+  const opciones = {
+    quality: 50,
+    lossless: false,
+    speed: 0,
+    chromaSubsampling: "4:2:0",
+  };
+
+  return src("src/img/**/*.{png,jpg}")
     .pipe(avif(opciones))
-    .pipe(dest('build/img'))
+    .pipe(dest("build/img"));
 }
 function dev() {
   /* watch("src/scss/header/_header.scss");*/
@@ -45,4 +48,4 @@ exports.dev = dev;
 exports.imageneswebp = imageneswebp;
 exports.imagenesavif = imagenesavif;
 exports.imagenes = imagenes;
-exports.default = series( css, dev);
+exports.default = series(css, dev);
